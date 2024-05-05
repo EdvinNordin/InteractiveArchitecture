ffd0ff9382afcbe3f167eb2830381debafd0f501
EdvinNordin <nordin.edvin@gmail.com>
2024-05-05 22:43:25 +0200

    gh-pages is working!

--- src/main.js
+++ src/main.js
@@ -5,7 +5,7 @@ import { camera, scene, renderer, world, composer } from "./Setup.js";
 import * as createObject from "./Objects.js";
 import { move } from "./Inputs.js";
 
-import { Rhino3dmLoader } from "./3DMLoader.js";
+import { Rhino3dmLoader } from "../node_modules/three/examples/jsm/loaders/3DMLoader.js";
 
 THREE.Object3D.DEFAULT_UP.set(0, 0, 1);
 
