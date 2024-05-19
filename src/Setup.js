9baff282a2395969196c5a0c3f01adf20669a8b9
EdvinNordin <nordin.edvin@gmail.com>
2024-05-19 17:24:57 +0200

    Single sided no Moira

--- src/Setup.js
+++ src/Setup.js
@@ -18,7 +18,7 @@ const world = new CANNON.World({ gravity: new CANNON.Vec3(0, -9.82, 0) });
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-renderer.toneMapping = THREE.ACESFilmicToneMapping;
+//renderer.toneMapping = THREE.ACESFilmicToneMapping;
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
@@ -32,18 +32,20 @@ camera.position.set(0, 8, 110);
 
 let dirLight = new THREE.DirectionalLight(0xffffff, 1);
 dirLight.castShadow = true;
-dirLight.shadow.mapSize.width = 2048;
-dirLight.shadow.mapSize.height = 2048;
 dirLight.shadow.camera.top = 50;
 dirLight.shadow.camera.bottom = -50;
 dirLight.shadow.camera.left = -50;
 dirLight.shadow.camera.right = 50;
+dirLight.shadow.bias = -0.0001;
 scene.add(dirLight);
 //const helper = new THREE.DirectionalLightHelper(dirLight, 1);
 //scene.add(helper);
 
-let ambientLight = new THREE.AmbientLight(0xffffff, 0.2);
-scene.add(ambientLight);
+const hemiLight = new THREE.HemisphereLight(0xffffff, 0xffffff, 0.1);
+hemiLight.color.setHSL(0.6, 1, 0.6);
+hemiLight.groundColor.setHSL(0.095, 1, 0.75);
+hemiLight.position.set(0, 50, 0);
+scene.add(hemiLight);
 
 window.addEventListener("resize", onWindowResize);
 
@@ -65,7 +67,7 @@ function initSky() {
     rayleigh: 0.2,
     mieCoefficient: 0.005,
     mieDirectionalG: 0.7,
-    elevation: 25,
+    elevation: 30,
     azimuth: 80,
     exposure: renderer.toneMappingExposure,
   };
@@ -91,22 +93,22 @@ function initSky() {
 function initSAO() {
   composer = new EffectComposer(renderer);
   renderPass = new RenderPass(scene, camera);
-  composer.addPass(renderPass);
+  //composer.addPass(renderPass);
   saoPass = new SAOPass(scene, camera);
-  composer.addPass(saoPass);
+  //composer.addPass(saoPass);
   const outputPass = new OutputPass();
-  composer.addPass(outputPass);
+  //composer.addPass(outputPass);
 
   saoPass.params.saoBias = 0.5;
-  saoPass.params.saoIntensity = 0.02;
-  saoPass.params.saoScale = 1;
-  saoPass.params.saoKernelRadius = 100;
+  saoPass.params.saoIntensity = 0.0012;
+  saoPass.params.saoScale = 0.3;
+  saoPass.params.saoKernelRadius = 40;
   saoPass.params.saoMinResolution = 0;
-  saoPass.params.saoBlur = false;
-  saoPass.params.saoBlurRadius = 8;
+  saoPass.params.saoBlur = true;
+  saoPass.params.saoBlurRadius = 16;
   saoPass.params.saoBlurStdDev = 4;
   saoPass.params.saoBlurDepthCutoff = 0.01;
-  saoPass.enabled = true;
+  saoPass.enabled = false;
 }
 
 function onWindowResize() {
--- src/main.js
+++ src/main.js
@@ -1,61 +1,56 @@
-import * as THREE from "three"; //"https://cdnjs.cloudflare.com/ajax/libs/three.js/0.164.1/three.module.js";
+import * as THREE from "three";
 import * as CANNON from "cannon-es";
 import CannonDebugger from "cannon-es-debugger";
 
 import { camera, scene, renderer, world, composer } from "./Setup.js";
 import * as createObject from "./Objects.js";
-//import { move, cameraBody } from "./Inputs.js";
 
 import { Rhino3dmLoader } from "../node_modules/three/examples/jsm/loaders/3DMLoader.js";
 
-THREE.Object3D.DEFAULT_UP.set(0, 0, 1);
+const cannonDebugger = new CannonDebugger(scene, world);
 
 let cameraBody = new CANNON.Body({
   mass: 1,
   shape: new CANNON.Sphere(1.5),
-  position: new CANNON.Vec3(0, 1, 50),
+  position: new CANNON.Vec3(0, 1.5, 50),
 });
-
-// Created objects
-let sphereMesh = createObject.newObject("Sphere").threeMesh;
-let sphereBody = createObject.newObject("Sphere").cannonBody;
-//scene.add(sphereMesh);
-//world.addBody(sphereBody);
+cameraBody.angularDamping = 1;
+cameraBody.linearDamping = 0.5;
+world.addBody(cameraBody);
+let raiser = new CANNON.Vec3(0, 7.5, 0);
 
 let planeMesh = createObject.newObject("Plane").threeMesh;
 let planeBody = createObject.newObject("Plane").cannonBody;
-
-const texture = new THREE.TextureLoader().load("assets/grass.jpg");
+const texture = new THREE.TextureLoader().load("assets/stacked-stones.jpg");
 texture.wrapS = THREE.RepeatWrapping;
 texture.wrapT = THREE.RepeatWrapping;
-texture.repeat.set(100, 100);
-
+texture.repeat.set(50, 50);
 planeMesh.material = new THREE.MeshStandardMaterial({
   map: texture,
   side: THREE.DoubleSide,
 });
-
 scene.add(planeMesh);
 world.addBody(planeBody);
 
 const loader = new Rhino3dmLoader();
-
 loader.setLibraryPath("https://cdn.jsdelivr.net/npm/rhino3dm@8.4.0/");
 loader.load(
   "assets/safe_space.3dm",
   function (object) {
-    object.up = new THREE.Vector3(0, 1, 0);
     object.rotation.x = -Math.PI / 2; // rotate the model
+
     scene.add(object);
-    //console.log(object);
-    //object.scale.set(0.01, 0.01, 0.01);
 
     let box = new THREE.Box3().setFromObject(object, false);
     let center = new THREE.Vector3();
     box.getCenter(center);
-
+    let mat;
     object.traverse((child) => {
       if (child.isMesh) {
+        child.material.side = 0;
+        child.recieveShadow = true;
+        child.castShadow = true;
+        //if (child.material.name === "Paint (4)") child.material.side = 0;
         let verts = child.geometry.attributes.position.array;
         for (let i = 0; i < verts.length; i++) {
           verts[i] = verts[i] * 0.01;
@@ -64,10 +59,11 @@ loader.load(
 
         let trimeshShape = new CANNON.Trimesh(verts, indis);
         let trimeshBody = new CANNON.Body({ mass: 0 });
+        trimeshBody.addShape(trimeshShape);
+
         let rot = new CANNON.Quaternion();
         rot.setFromAxisAngle(new CANNON.Vec3(1, 0, 0), -Math.PI / 2);
         trimeshBody.quaternion = rot;
-        trimeshBody.addShape(trimeshShape);
 
         world.addBody(trimeshBody);
       }
@@ -81,60 +77,15 @@ loader.load(
   }
 );
 
-const cannonDebugger = new CannonDebugger(scene, world);
-let raiser = new CANNON.Vec3(0, 7.5, 0);
-let test = new THREE.Vector3();
-// Start the simulation loop
-function animate() {
-  //loops the animate function
-  requestAnimationFrame(animate);
-  // Step the physics world
-  world.fixedStep();
-  // Move the camera
-  move();
-
-  camera.position.copy(cameraBody.position.vadd(raiser));
-  camera.quaternion.copy(cameraBody.quaternion);
-  cannonToThree();
-  //cannonDebugger.update();
-  // Render three.js
-  renderer.render(scene, camera);
-
-  // Render the effect composer (post processing)
-  //composer.render();
-}
-
-// Copy coordinates from Cannon.js to Three.js
-function cannonToThree() {
-  //camera.position.copy(cameraBody.position);
-  //camera.quaternion.copy(cameraBody.quaternion);
-  //console.log(cameraBody.quaternion);
-  // Pos then rot
-  //sphereMesh.position.copy(sphereBody.position);
-  //sphereMesh.quaternion.copy(sphereBody.quaternion);
-  //scene.children.forEach((child) => {});
-}
-
-let forwardBool,
-  backwardBool,
-  leftBool,
-  rightBool,
-  upBool,
-  downBool,
-  flyingBool = false;
-
-const instructions = document.getElementById("instructions");
+document.body.onclick = () => {
+  if (document.pointerLockElement !== document.body) {
+    document.body.requestPointerLock();
+  }
+};
 
 let phi_ = 0;
 let theta_ = 0;
-
 let qx = new CANNON.Quaternion();
-
-cameraBody.angularDamping = 1;
-cameraBody.linearDamping = 0.5;
-
-world.addBody(cameraBody);
-
 document.addEventListener("mousemove", (e) => {
   if (document.pointerLockElement === document.body) {
     const xh = e.movementX * 0.0005;
@@ -149,19 +100,12 @@ document.addEventListener("mousemove", (e) => {
 
     let q = new CANNON.Quaternion(0, 0, 0, 0);
     q = qx.mult(qz);
-    //qx.mult(qz);
-    //console.log(q);
 
     cameraBody.quaternion.copy(q);
   }
 });
 
-document.body.onclick = () => {
-  if (document.pointerLockElement !== document.body) {
-    document.body.requestPointerLock();
-  }
-};
-
+const instructions = document.getElementById("instructions");
 document.addEventListener("pointerlockchange", (e) => {
   if (document.pointerLockElement === document.body) {
     instructions.style.display = "none";
@@ -170,6 +114,13 @@ document.addEventListener("pointerlockchange", (e) => {
   }
 });
 
+let forwardBool,
+  backwardBool,
+  leftBool,
+  rightBool,
+  upBool,
+  downBool,
+  flyingBool = false;
 document.addEventListener("keydown", (e) => {
   if (document.pointerLockElement === document.body) {
     if (e.keyCode == 87) forwardBool = true;
@@ -189,7 +140,7 @@ document.addEventListener("keydown", (e) => {
         cameraBody.mass = 1;
         cameraBody.collisionFilterGroup = 1;
         world.gravity.set(0, -9.82, 0);
-        cameraBody.position.y = 8;
+        cameraBody.position.y = 1.5;
       }
     }
   }
@@ -211,88 +162,60 @@ function move() {
     if (forwardBool) {
       movement = new CANNON.Vec3(0, 0, -speed);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.velocity = cameraBody.velocity.vadd(movement);
-      //console.log(cameraBody);
-      //cameraBody.position += forwardMovement;
     }
     if (backwardBool) {
       let movement = new CANNON.Vec3(0, 0, speed);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.velocity = cameraBody.velocity.vadd(movement);
-      //cameraBody.position = cameraBody.position.vadd(backwardMovement);
-      //cameraBody.position += backwardMovement;
     }
     if (leftBool) {
       let movement = new CANNON.Vec3(-speed, 0, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
       cameraBody.velocity = cameraBody.velocity.vadd(movement);
     }
     if (rightBool) {
       let movement = new CANNON.Vec3(speed, 0, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.velocity = cameraBody.velocity.vadd(movement);
-      //cameraBody.position = cameraBody.position.vadd(rightMovement);
-      //cameraBody.position += rightMovement;
     }
   } else {
     if (forwardBool) {
       movement = new CANNON.Vec3(0, 0, -1);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.position = cameraBody.position.vadd(movement);
     }
     if (backwardBool) {
       let movement = new CANNON.Vec3(0, 0, 1);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.position = cameraBody.position.vadd(movement);
     }
     if (leftBool) {
       let movement = new CANNON.Vec3(-1, 0, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
       cameraBody.position = cameraBody.position.vadd(movement);
     }
     if (rightBool) {
       let movement = new CANNON.Vec3(1, 0, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
-
       cameraBody.position = cameraBody.position.vadd(movement);
     }
 
     if (upBool) {
       let movement = new CANNON.Vec3(0, 1, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
       cameraBody.position = cameraBody.position.vadd(movement);
-      //cameraBody.velocity = cameraBody.velocity.vadd(movement);
     }
     if (downBool) {
       let movement = new CANNON.Vec3(0, -1, 0);
-      //movement.scale(speed, movement);
       movement = applyQuaternion(movement, qx);
       cameraBody.position = cameraBody.position.vadd(movement);
-
-      //cameraBody.velocity = cameraBody.velocity.vadd(movement);
     }
   }
   if (cameraBody.velocity.length() > 20) {
     cameraBody.velocity.normalize();
     cameraBody.velocity.scale(20, cameraBody.velocity);
   }
-  /*
-  if (cameraBody.velocity.x > 20) cameraBody.velocity.x = 20;
-  else if (cameraBody.velocity.x < -20) cameraBody.velocity.x = -20;
-  if (cameraBody.velocity.y > 20) cameraBody.velocity.y = 20;
-  else if (cameraBody.velocity.y < -20) cameraBody.velocity.y = -20;
-  if (cameraBody.velocity.z > 20) cameraBody.velocity.z = 20;
-  else if (cameraBody.velocity.z < -20) cameraBody.velocity.z = -20;*/
 }
 
 function clamp(x, a, b) {
@@ -322,4 +245,35 @@ function applyQuaternion(vec, quat) {
   return vec;
 }
 
+function cannonToThree() {
+  camera.position.copy(cameraBody.position.vadd(raiser));
+  camera.quaternion.copy(cameraBody.quaternion);
+  //camera.position.copy(cameraBody.position);
+  //camera.quaternion.copy(cameraBody.quaternion);
+  //console.log(cameraBody.quaternion);
+  // Pos then rot
+  //sphereMesh.position.copy(sphereBody.position);
+  //sphereMesh.quaternion.copy(sphereBody.quaternion);
+  //scene.children.forEach((child) => {});
+}
+
 animate();
+function animate() {
+  //loops the animate function
+  requestAnimationFrame(animate);
+
+  // Step the physics world
+  world.fixedStep();
+
+  // Move the camera
+  move();
+
+  cannonToThree();
+  //cannonDebugger.update();
+
+  // Render three.js
+  renderer.render(scene, camera);
+
+  // Render the effect composer (post processing)
+  composer.render();
+}
