00c8e4205006ff060581eef55a9388c0ecc868c1
EdvinNordin <nordin.edvin@gmail.com>
2025-01-08 22:12:26 +0100

    test

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -12,6 +12,14 @@ export default defineConfig({
             Buffer: ['buffer', 'Buffer'], // Automatically replace 'Buffer' with the polyfill
         }),
     ],
+    resolve: {
+        alias: {
+            // Polyfill for other missing modules (if necessary)
+            stream: 'stream-browserify',
+            crypto: 'crypto-browserify',
+            path: 'path-browserify',
+        },
+    },
 });
 /*
 import { defineConfig } from 'vite';
