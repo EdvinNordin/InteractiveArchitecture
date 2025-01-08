96fc604df508378b3faebd21cd28b76107f7590c
EdvinNordin <nordin.edvin@gmail.com>
2025-01-08 19:22:52 +0100

    testing

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,8 +1,10 @@
 import {defineConfig} from "vite";
+import {rmSync} from "fs"; // Node.js file system module
 
 export default defineConfig({
-    base: "/Client", // Keep this as "/" if this is deployed to the root
+    base: "/Client/", // Keep this as "/" if this is deployed to the root
     build: {
         outDir: "dist", // Default output directory (can be changed)
+        emptyOutDir: true, // Automatically clears the output directory
     },
 });
